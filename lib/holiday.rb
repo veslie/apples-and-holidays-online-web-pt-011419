7878ad2f0e9f20c757dcb694f9785e7fbbd2137a
veslie losius <veslielosius@yahoo.com>
2019-02-14 03:28:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   }
   def
   second_supply_for_fourth_of_july(holiday_supplies)
-  
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
