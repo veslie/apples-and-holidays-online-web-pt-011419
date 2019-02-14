c7aa62c9356d3158df961e41aa300bc8dc2e493a
veslie losius <veslielosius@yahoo.com>
2019-02-14 03:51:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
+  
   # include the string "BBQ"
 
 end
