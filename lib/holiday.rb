4db458da7780dd6f47449fc587bec933b338a0ce
veslie losius <veslielosius@yahoo.com>
2019-02-14 03:27:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
      }
   }
   def
-  return the second element in the 4th of July array
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
